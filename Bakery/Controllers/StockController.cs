8211520b815aea28d1d9a51c526eeb284fe584e4
emil874g <emilbdl@gmail.com>
2024-04-04 14:08:08 +0200

    Added CRUD operations

--- Bakery/Controllers/StockController.cs
+++ Bakery/Controllers/StockController.cs
@@ -40,10 +40,7 @@ namespace Bakery.Controllers
             .Select(bs => new StockDTO
             {
                 Ingredient = bs.Stock.Name,
-                Quantity = bs.Quantity,
-                Allergens = bs.Stock.StockAllergens
-                      .Select(sa => sa.Allergen.AllergenName)
-                      .ToList()
+                Quantity = bs.Quantity
             })
             .ToListAsync();
 
@@ -63,6 +60,150 @@ namespace Bakery.Controllers
             };
 
             return Ok(restDTO);
+        }
+
+        //POST
+        [HttpPost(Name = "AddStock")]
+        [ResponseCache(NoStore = true)]
+        public async Task<IActionResult> AddStock(StockDTO stockDto)
+        {
+            try
+            {
+                if (stockDto.Quantity < 0)
+                {
+                    return BadRequest("Quantity cannot be negative.");
+                }
+
+                var stock = new Stock
+                {
+                    Name = stockDto.Ingredient,
+                    Quantity = stockDto.Quantity
+                };
+
+                _context.Stocks.Add(stock);
+                await _context.SaveChangesAsync();
+
+                var ListOfStocks = await _context.Stocks
+                    .Where(s => s.Name == stock.Name)
+                    .ToListAsync();
+
+                return Ok(new RestDTO<Stock>()
+                {
+                    Data = ListOfStocks,
+                    Links = new List<LinkDTO>
+                {
+                    new LinkDTO(
+                            Url.Action(
+                                null,
+                                "Stock",
+                                new {stock.Name},
+                                Request.Scheme)!,
+                            "self",
+                            "POST"),
+                }
+                });
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(CustomLogEvents.Stock_Post, ex, "Error in Post method.");
+                return StatusCode(500, "Internal server error.");
+            }
+
+        }
+
+
+
+        // PUT
+        [HttpPut(Name = "UpdateStock")]
+        [ResponseCache(NoStore = true)]
+        public async Task<IActionResult> UpdateStock(StockDTO stockDto)
+        {
+            try
+            {
+                if (stockDto.Quantity < 0)
+                {
+                    return BadRequest("Quantity cannot be negative.");
+                }
+
+                var stock = await _context.Stocks
+                    .Where(s => s.Name == stockDto.Ingredient)
+                    .FirstOrDefaultAsync();
+
+                if (stock == null)
+                {
+                    return NotFound("Stock not found.");
+                }
+                stock.Quantity = stockDto.Quantity;
+
+                _context.Stocks.Update(stock);
+                await _context.SaveChangesAsync();
+
+                return Ok(new RestDTO<Stock>()
+                {
+                    Data = new List<Stock> { stock },
+                    Links = new List<LinkDTO>
+                {
+                    new LinkDTO(
+                            Url.Action(
+                                null,
+                                "Stock",
+                                new {stock.Name},
+                                Request.Scheme)!,
+                            "self",
+                            "PUT"),
+                }
+                });
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(CustomLogEvents.Stock_Put, ex, "Error in Put method.");
+                return StatusCode(500, "Internal server error.");
+            }
+        }
+
+        // DELETE
+        [HttpDelete(Name = "DeleteStock")]
+        [ResponseCache(NoStore = true)]
+        public async Task<IActionResult> DeleteStock(string ingredient)
+        {
+            try
+            {
+                var stock = await _context.Stocks
+                    .Where(s => s.Name == ingredient)
+                    .FirstOrDefaultAsync();
+
+                if (stock == null)
+                {
+                    return NotFound("Stock not found.");
+                }
+
+                _context.Stocks.Remove(stock);
+                await _context.SaveChangesAsync();
+
+                return Ok(new RestDTO<Stock>()
+                {
+                    Data = new List<Stock> { stock },
+                    Links = new List<LinkDTO>
+                {
+                    new LinkDTO(
+                            Url.Action(
+                                null,
+                                "Stock",
+                                new {stock},
+                                Request.Scheme)!,
+                            "self",
+                            "DELETE"),
+                }
+                });
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(CustomLogEvents.Stock_Delete, ex, "Error in Delete method.");
+                return StatusCode(500, "Internal server error.");
+            }
         }
 
     }
